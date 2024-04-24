81f62ab865c615065d13ed01ef90693d247c0700
Miaowen <66027205+dmw1998@users.noreply.github.com>
2024-04-24 14:23:44 +0200

    Update MH_MCMC_Sampling_1D.py
    Changed samples' initial type

--- MH_MCMC_Sampling_1D.py
+++ MH_MCMC_Sampling_1D.py
@@ -1,71 +1,58 @@
-from MH_MCMC_Sampling_1D import metropolis_hastings_1d
-from MH_MCMC_Sampling_1D import generate_seed
-from diffusion import diffusion
+# Metropolis-Hastings algorithm for 1D sampling
+
+# Choose a correlation parameter gamma
+# Given a seed theta_1, generate a sequence of theta_i's
+
 import numpy as np
-import matplotlib.pyplot as plt
 
-# How to determine the failure domain for the next level?
-# Try 1: Cut one side of the domain - How to decide which side to cut and the cut point?
-# Try 2: Cut both sides of the domain - How to decide the cut points?
-# For example 2, I keep the right side of the domain as the failure domain of the next level
+# # target distribution
+# def target_distribution(x):
+#     # one-dimensional Gaussian distribution
+#     mu = 0.0
+#     sigma = 1.0
+#     return np.exp(-0.5 * ((x - mu) / sigma)**2) / (sigma * np.sqrt(2 * np.pi))
 
-def failure_level(theta, u, u_max, p0=0.1, x_min=0.0, x_max=1.0):
-    # Cut both sides of the domain
-    # keep the failure points as the boundary of the failure domain of the next level
-    u_diff = u - u_max  # Differences u_h(1) - u_max
-    c1 = np.quantile(u_diff, p0)  # Calculate the p0 quantile of u_diff, for the failure domain
-    
-    print(u_diff <= c1)
-    print(theta)
-    
-    # F = {theta_i | u_h(theta_i) - u_max <= c1}
-    failure_domain = theta[u_diff <= c1]
-    
-    x_min = np.max(theta[u_diff > c1])  # Domain contains the failure points
+# proposal distribution
+def proposal_distribution(theta_n, gamma):
+    # multivariate normal distribution
+    return gamma * theta_n + np.sqrt(1 - gamma**2) * np.random.normal(0, 1)
 
-    return failure_domain, x_min, x_max
+# acceptance condition
+def acceptance_condition(theta_c, c_min, c_max):
+    # For the nested partial failure domain
+    return theta_c <= c_max and theta_c >= c_min
 
-def subset_simulation(p0, N, L, u_max, x_min, x_max):
-    # Generate N i.i.d samples theta_1, ..., theta_N ~ f() use in MC
-    theta = np.zeros(N)
-    for i in range(N):
-        theta[i] = generate_seed(x_min, x_max)
-        
-    # Determine a failure level c_1 > 0 s.t. P_1 = p_0
-    # Solve the diffusion equation with a = theta_i
-    u = np.zeros(N)
-    for i in range(N):
-        u_h = diffusion(theta[i], 10)
-        u[i] = u_h(1)
-        
-    theta, x_min, x_max = failure_level(theta, u, u_max, p0, x_min, x_max)
-    print(theta)
-    
-    for _ in range(1, L):
-        # Use the N_0 failure points in F_{l-1} as seeds and generate N - N_0 new samples theta_i ~ f(.|F_{l-1}) with MH_type_MCMC_sampling
-        N_0 = len(theta)
-        theta = metropolis_hastings_1d(theta, N - N_0 + 1, 0.8, x_min, x_max) # gamma = 0.8
-        
-        for i in range(N):
-            u_h = diffusion(theta[i], 10)
-            u[i] = u_h(1)
+def metropolis_hastings_1d(theta, N, gamma, c_min, c_max):
+    samples = []
+    for theta_1 in theta:
+        samples.append(theta_1)
+
+        for _ in range(1,N):
+            theta_c = proposal_distribution(theta_1, gamma)
             
-        theta, x_min, x_max = failure_level(theta, u, u_max, p0, x_min, x_max)
-        print(theta)
-        
-    # Evaluate the failure probability P_L = 1/N sum_{i=1}^N I(G(theta_i) <= c_L) and return P_F = P_1 \times \prod_{l=2}^L P_l
-    P_L = p0**(L-1) * np.mean(u <= u_max)
-    
-    return P_L
-    
-    
+            acceptance = acceptance_condition(theta_c, c_min, c_max)
+            
+            if acceptance:
+                theta_1 = theta_c
+
+            samples.append(theta_1)
+
+    return np.array(samples)
+
+def generate_seed(x_min=0.0, x_max=1.0):
+    while True:
+        theta = np.random.normal(0, 1)
+        if acceptance_condition(theta, x_min, x_max):
+            return theta
 
 if __name__ == '__main__':
-    p0 = 0.1        # Probability of failure
-    N = 10          # Number of samples
-    L = 10          # Number of levels
-    x_min = 0.0     # Minimum value of the random field
-    x_max = 1.0     # Maximum value of the random field
-    u_max = 0.535   # Maximum value of the solution
-    P_L = subset_simulation(p0, N, L, u_max, x_min, x_max)
-    print(P_L)
+    import matplotlib.pyplot as plt
+    N = 10
+    gamma = 0.5
+    c_min = 0.0
+    c_max = 1.0
+    int_seed = generate_seed() # std Gaussian
+    theta = [int_seed]
+    samples = metropolis_hastings_1d(theta, N, gamma, c_min, c_max)
+    plt.hist(samples, bins=50, density=True)
+    plt.show()
