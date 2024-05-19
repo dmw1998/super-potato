f77ebe5911b3fdbe5e34cbbea33499cbdb2cb91f
dmw <miaowen.dong@tum.de>
2024-05-19 10:27:49 +0200

    Considering the case of reaching the finest level
    On branch example_1
    Your branch is up to date with 'origin/example_1'.
    
    Changes to be committed:
    	modified:   IoQ_and_c_l.py
    
    Untracked files:
    	__pycache__/
    	subset_simulation.py

--- IoQ_and_c_l.py
+++ IoQ_and_c_l.py
@@ -65,6 +65,7 @@ def failure_level(G, thetas_ls, N, p0 = 0.1):
     # p0: failure probability
     
     # output:
+    # G, thetas_ls: updated G and thetas_ls
     # c_l: failure level
     
     sorted_indices = sorted(range(N), key=lambda k: G[k])
@@ -72,9 +73,13 @@ def failure_level(G, thetas_ls, N, p0 = 0.1):
     sorted_theta_ls = [thetas_ls[i] for i in sorted_indices]
     
     N0 = int(p0 * N)
-    G = sorted_G[:N0]
-    thetas_ls = sorted_theta_ls[:N0]
     c_l = sorted_G[N0-1]
+    if c_l < 0:
+        # When we reach the finest level
+        G = [g for g in G if g > 0]
+    else:
+        G = sorted_G[:N0]
+        thetas_ls = sorted_theta_ls[:N0]
     
     return G, thetas_ls, c_l
     
@@ -101,3 +106,4 @@ if __name__ == '__main__':
     print('c_l =', c_l)
     print('G =', len(G))
     print('thetas =', len(thetas))
+    print("failure probability:", len(G) / N)
