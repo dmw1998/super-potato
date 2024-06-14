2190e6daf95e7775a07275f630be252f29a9edab
dmw <miaowen.dong@tum.de>
2024-06-14 13:24:11 +0200

    95% confidence interval for failure probability: (0.00011610560800000001, 0.00016227850849999997) Changes to be committed: 	modified:   1d_example/subset_simulation_y_l.py
    Changes not staged for commit:
    	modified:   1d_example/multilevel_estimator.py
    
    Untracked files:
    	1d_example/__pycache__/
    	1d_example/adap_result.txt
    	1d_example/out.log
    	__pycache__/
    	out.log

--- 1d_example/subset_simulation_y_l.py
+++ 1d_example/subset_simulation_y_l.py
@@ -38,13 +38,22 @@ def subset_simulation_yl(N, M, u_max, n_grid, gamma, corr_coeff = 0.8, L = 5):
         thetas = np.random.normal(0, 1, M)
         u_1 = IoQ(kl_expan(thetas), n_grid)
         g = u_max - u_1
-        if g < y[0]:
-            theta_ls.append(thetas)
-            G.append(g)
+        theta_ls.append(thetas)
+        G.append(g)
             
+    theta_new = []
+    G_new = []
+    for index, value in enumerate(G):
+        if value < y[0]:
+            theta_new.append(theta_ls[index])
+            G_new.append(G[index])
+            
+    theta_ls = theta_new
+    G = G_new
+    
     p_f = len(G) / N
-    # print("p_f: ", p_f)
-            
+    # print("prob at 1 is", p_f)
+    
     for l in range(1, L):
         # Generate N - N0 samples for each level
         G_c, theta_c_ls = sampling_theta_list(N, G, theta_ls, y[l-1], u_max, n_grid, corr_coeff)
@@ -56,11 +65,28 @@ def subset_simulation_yl(N, M, u_max, n_grid, gamma, corr_coeff = 0.8, L = 5):
             if g < y[l]:
                 theta_ls.append(theta_c_ls[i])
                 G.append(g)
-                
-        if len(G) == 0:
-            # print("No sample in the failure domain F_{", l+1, "}")
-            s = False
-            break
+        
+        k = 0     
+        while len(G) == 0:
+            # Resample the initial states
+            theta_ls = [theta_c_ls[-1]]
+            G = [G_c[-1]]
+            G_c, theta_c_ls = sampling_theta_list(N, G, theta_ls, y[l-1], u_max, n_grid, corr_coeff)
+            
+            theta_ls = []
+            G = []
+            for i in range(N):
+                g = G_c[i]
+                if g < y[l]:
+                    theta_ls.append(theta_c_ls[i])
+                    G.append(g)
+            k += 1
+            # print("Zero probability of failure at level:", l, "i:", k)
+            
+            if k > 2000:
+                s = False
+                print("Zero probability of failure at level:", l, "i:", k)
+                break
         
         # print("prob at", l+1, "is", len(G) / N)
         p_f *= len(G) / N
@@ -68,33 +94,34 @@ def subset_simulation_yl(N, M, u_max, n_grid, gamma, corr_coeff = 0.8, L = 5):
     return s, p_f
 
 if __name__ == "__main__":
-    N = 10
+    N = 1000
     M = 150
     u_max = 0.535
-    n_grid = 32
+    n_grid = 100
     gamma = 0.13
     corr_coeff = 0.8
-    L = 7
+    L = 5
     
     y = y_l(L, gamma)
-    print("y: ", y)
+    # print("y: ", y)
     
     np.random.seed(0)
-    # s, p_f = subset_simulation_yl(N, M, u_max, n_grid, gamma, corr_coeff, L)
-    # print("failure probability{:.2e}".format(p_f))
+    s, p_f = subset_simulation_yl(N, M, u_max, n_grid, gamma, corr_coeff, L)
+    print("failure probability  {:.2e}".format(p_f))
     s = False
     p_f = np.zeros(100)
     for i in range(100):
         ind = i
         while True:
             s, p = subset_simulation_yl(N, M, u_max, n_grid, gamma, corr_coeff, L)
+            # print("s", s)
             if s:
                 p_f[ind] = p
-                # print("failure probability {:.2e}".format(p_f[ind]))
+                print("failure probability {:.2e}".format(p_f[ind]))
                 break
         
-    p_f_mean = np.mean(p_f)
-    print("mean of failure probability {:.2e}".format(p_f_mean))
+    # p_f_mean = np.mean(p_f)
+    # print("mean of failure probability {:.2e}".format(p_f_mean))
     
     from subset_simulation import bootstrap_confidence_interval
 
@@ -102,3 +129,17 @@ if __name__ == "__main__":
     confidence_interval = bootstrap_confidence_interval(p_f, num_bootstrap_samples=100, confidence_level=0.95)
 
     print("95% confidence interval for failure probability:", confidence_interval)
+    
+    p_f = sorted(p_f)
+    cdf = np.arange(1, len(p_f) + 1) / len(p_f)
+
+    # Step 3: Plot the empirical CDF
+    plt.figure(figsize=(8, 6))
+    plt.xscale("log")
+    plt.xlim(1e-5, 1e-3)
+    plt.step(p_f, cdf, where='post')
+    plt.xlabel('Probability')
+    plt.ylabel('Empirical CDF')
+    plt.title('Empirical CDF of Probabilities')
+    plt.grid(True)
+    plt.show()
