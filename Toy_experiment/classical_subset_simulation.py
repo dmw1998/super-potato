4944e604b4b0fd58c6aeffe44e067ac50a450eba
dmw <miaowen.dong@tum.de>
2024-06-23 10:50:10 +0200

    95% confidence interval for failure probability: (7.07e-05, 7.37e-05) 90% confidence interval for failure probability: (7.34e-05, 7.09e-05)
    Why the 95% confidence interval is wider than the 90% confidence interval?

--- Toy_experiment/classical_subset_simulation.py
+++ Toy_experiment/classical_subset_simulation.py
@@ -19,9 +19,10 @@ def classical_subset_simulation(N, y_L = -3.8, p0 = 0.1, gamma = 0.5, L = 5):
     # gamma: auto-correlation factor
     # L: total number of levels
     
+    
     # Initialization, l= 1
     G = np.random.normal(0, 1, N)
-    kappa = np.random.choice([-1, 1], N)
+    kappa = np.array([k(g) for g in G])
     G_l = G + kappa * gamma
     
     # Compute the probability threshold
@@ -33,39 +34,37 @@ def classical_subset_simulation(N, y_L = -3.8, p0 = 0.1, gamma = 0.5, L = 5):
     
     mask = G_l <= c_l
     G_l = G_l[mask]
-    # kappa = kappa[mask]
     
     for l in range(2, L):
-        G_l, kappa = Generate_G_l(G_l, kappa, N, l, c_l, gamma = gamma)
-        # G_l, kappa = modified_metropolis_hastings(G_l, kappa, N, l, c_l, gamma = gamma)
-        # G_l = sample_new_G(G_l, N, l, c_l)
+        # G_l = Generate_G_l(G_l, N, l, c_l, gamma = gamma)
+        # G_l = modified_metropolis_hastings(G_l, N, l, c_l, gamma = gamma)
+        G_l = sample_new_G(G_l, N, l, c_l, gamma = gamma)
         
         c_l = sorted(G_l)[int(N*p0)]
         # print("The probability threshold for level", l, "is", c_l)
         
         if c_l <= y_L:
             mask = G_l <= y_L
-            return p0 ** l * np.sum(mask) / N
+            return p0 ** (l-1) * np.sum(mask) / N
         
         mask = G_l <= c_l
         G_l = G_l[mask]
-        # kappa = kappa[mask]
         
-    G_l, kappa = Generate_G_l(G_l, kappa, N, L, c_l, gamma = gamma)
-    # G_l, kappa = modified_metropolis_hastings(G_l, kappa, N, l, c_l, gamma = gamma)
-    # G_l = sample_new_G(G_l, N, l, c_l)
+    # G_l= Generate_G_l(G_l, N, L, c_l, gamma = gamma)
+    # G_l = modified_metropolis_hastings(G_l, N, l, c_l, gamma = gamma)
+    G_l = sample_new_G(G_l, N, l, c_l, gamma = gamma)
     mask = G_l <= y_L
     # print("The number of samples in the failure domain is", np.sum(mask))
     return p0 ** (L-1) * np.sum(mask) / N
 
 if __name__ == "__main__":
-    N = 70  # Total number of samples per level
-    p_0 = 0.13  # Probability threshold for each subset, 0.13^5 = 7.23e-05
+    N = 1000  # Total number of samples per level
+    p_0 = 0.1  # Probability threshold for each subset, 0.15^5 = 7.59e-05
     gamma = 0.5
     L = 5  # Total number of levels
     y_L = -3.8  # Failure threshold
     
-    np.random.seed(0)
+    np.random.seed(2)
     # p_f = classical_subset_simulation(N, p0=p_0, L=L)
     # print("The failure p_fability is {:.2e}".format(p_f))
     
@@ -75,9 +74,11 @@ if __name__ == "__main__":
     # print("Failure probabilities:", failure_probabilities[0:10])
 
     # Calculate 95% confidence interval using bootstrap method
-    confidence_interval = bootstrap_confidence_interval(failure_probabilities, num_bootstrap_samples=1000, confidence_level=0.95)
+    confidence_interval, ci = bootstrap_confidence_interval(failure_probabilities, num_bootstrap_samples=1000)
 
     print("95% confidence interval for failure probability: ({:.2e}, {:.2e})".format(confidence_interval[0], confidence_interval[1]))
+    
+    print("90% confidence interval for failure probability: ({:.2e}, {:.2e})".format(ci[0], ci[1]))
     
     p_f = sorted(failure_probabilities)
     cdf = np.arange(1, len(p_f) + 1) / len(p_f) 
@@ -88,26 +89,14 @@ if __name__ == "__main__":
     # plt.xlim(1e-5, 1e-3)
     plt.step(p_f, cdf, where='post')
     plt.axvline(7.23e-05, color='r', linestyle='--', label='True Value')
-    plt.axvline(confidence_interval[0], color='g', linestyle='--', label='95% Confidence Interval')
-    plt.axvline(confidence_interval[1], color='g', linestyle='--')
+    plt.axvline(confidence_interval[0], color='g', alpha = 0.5, linestyle='--', label='95% Confidence Interval')
+    plt.axvline(confidence_interval[1], color='g', alpha = 0.5, linestyle='--')
+    plt.axvline(ci[0], color='m', alpha = 0.5, linestyle='--', label='90% Confidence Interval')
+    plt.axvline(ci[1], color='m', alpha = 0.5, linestyle='--')
     plt.xlabel('Probability')
     plt.ylabel('Empirical CDF')
     plt.title('Empirical CDF of Probabilities')
+    plt.legend()
     plt.grid(True)
     plt.show()
     
-    # N_ls = [50, 100, 1000, 2000, 5000, 10000]
-    # p_0_ls = [0.1, 0.13, 0.15]
-    # L_ls = [5, 6]
-    
-    # with open("subset_simulation_results.txt", "w") as f:
-    #     for N in N_ls:
-    #         for p_0 in p_0_ls:
-    #             for L in L_ls:
-    #                 f.write("N = {}, p0 = {}, L = {}\n".format(N, p_0, L))
-                    
-    #                 failure_probabilities = [classical_subset_simulation(N, p0=p_0, L=L, y_L=y_L) for _ in range(1000)]
-                    
-    #                 confidence_interval = bootstrap_confidence_interval(failure_probabilities, num_bootstrap_samples=1000, confidence_level=0.95)
-                    
-    #                 f.write("95% confidence interval for failure probability: ({:.2e}, {:.2e})\n\n".format(confidence_interval[0], confidence_interval[1]))
